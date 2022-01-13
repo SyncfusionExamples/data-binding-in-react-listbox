b03fa816d68a7f8abb411ecad099ab9d03901726
vinoth-Kumar-S <31274282+vinoth-Kumar-S@users.noreply.github.com>
2022-01-13 10:13:21 +0530

    Adding React ListBox Data Binding Sample

---
+++ src/data.js
@@ -0,0 +1,79 @@
+
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.employees=[
+    { 
+        "EmployeeID": "7", 
+        "EmpDetails": {
+            "Name": "Andrew Fuller", 
+            "Designation": "Team Lead", 
+            "Country": "England"
+        }, 
+    },
+    { 
+        "EmployeeID": "1", 
+        "EmpDetails": {
+            "Name": "Anne Dodsworth",
+            "Designation": "Developer", 
+            "Country": "USA" 
+        },
+    },
+    { 
+        "EmployeeID": "3", 
+        "EmpDetails": {
+            "Name": "Janet Leverling", 
+            "Designation": "HR", 
+            "Country": "USA" 
+        },
+    },
+    { 
+        "EmployeeID": "2", 
+        "EmpDetails": {
+            "Name": "Laura Callahan",
+            "Designation": "Product Manager", 
+            "Country": "USA"
+        }, 
+    },
+    {  
+        "EmployeeID": "6", 
+        "EmpDetails": {
+            "Name": "Margaret Peacock",
+            "Designation": "Developer", 
+            "Country": "USA"
+        },
+         
+    },
+    {  
+        "EmployeeID": "9",
+        "EmpDetails": {
+            "Name": "Michael Suyama",
+            "Designation": "Team Lead", 
+            "Country": "USA"
+        } 
+    },
+    {  
+        "EmployeeID": "4",
+        "EmpDetails": { 
+            "Name": "Nancy Davolio",
+            "Designation": "Product Manager", 
+            "Country": "USA" 
+        }
+    },
+    {    
+        "EmployeeID": "8", 
+        "EmpDetails": {
+            "Name": "Robert King",
+            "Designation": "Developer ", 
+            "Country": "England"
+        },
+    },
+    {  
+        "EmployeeID": "10",
+        "EmpDetails": {
+            "Name": "Steven Buchanan",
+            "Designation": "CEO", 
+            "Country": "England" 
+        }, 
+    }
+]
+
+exports.employees1=[];
